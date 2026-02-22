42f7be88a96f68ea40aa12694f2564fdc0f82193
“KhalatyanVictor” <viktorkhalatyan2008@gmail.com>
2026-02-22 14:47:42 +0400

    init: create Next.js app with TypeScript

---
+++ src/components/selectComponents/select.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Select = () => {
+  return (
+    <div>
+      <select title="Select a fruit">
+        <option>Banana</option>
+        <option>Apple</option>
+      </select>
+    </div>
+  );
+};
+
+export default Select;
